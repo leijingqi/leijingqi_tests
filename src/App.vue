fcf6f90e1b7f105a89f9ef43de1b5f32426c452a
leijingqi <9840031+leijingqi@user.noreply.gitee.com>
2022-08-25 20:42:27 +0800

    setup参数

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>{{a}}</div>
+  <h1>{{user.name}}</h1>
+  <h1>{{user.age}}</h1>
+  <h2>userVaue{{userVaue.age}}</h2>
+  <h2>userVaue{{userVaue.list}}</h2>
+  <button @click="upDataA">更新数据</button>
+  <Child  name='name' @jj='jj'/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, ref } from 'vue';
+import Child from './components/Child.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Child},
+  setup(props) { 
+    function jj(name:string){
+      user.name+=name
+    }
+    let  a = ref(0)  //ref函数返回的是一个ref对象需要。value操作  模板中不需要 架子自动取去。value了
+    let obj = {
+      name:'zhao',
+      age:14,
+      list:['11','22']
+    }
+    let user = reactive({
+      name:'雷',
+      age:18,
+      car:['奥托']
+    })
+    let userVaue = reactive(obj)
+    function upDataA(){
+          a.value+=1,
+          user.age++,
+          userVaue.age+=1
+          userVaue.list[4]='999'
+    }
+    return {
+      a,
+      upDataA,
+      user,
+      userVaue,
+      jj
+    }
   }
 });
 </script>
---
+++ src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>雷jingqi</h1>
+  <button @click="childHandler">子组件触发事件</button>
+</div>
+</template>
+<script>
+// 解构
+import { defineComponent,} from 'vue'
+// 模块化
+export default defineComponent({
+name:'', 
+props: ['name'],
+components: {
+},
+setup(props, ctx){
+  console.log(props,ctx);
+  function childHandler(){
+    ctx.emit('jj','++')
+  }
+return {
+  childHandler
+}
+}
+})
+</script>
+<style scoped>
+</style>
+ 
